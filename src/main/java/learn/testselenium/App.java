1602ef54ee594135f2efff0f0f3eaabdba96f4fb
sandeep <sandip89ster@gmail.com>
2016-08-19 17:57:39 +0530

    Test

---
+++ src/main/java/learn/testselenium/App.java
@@ -0,0 +1,15 @@
+package learn.testselenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class App 
+{
+	@Test
+    public static void test1()
+    {
+       WebDriver driver = new FirefoxDriver();
+       driver.get("http://www.google.com");
+    }
+}
